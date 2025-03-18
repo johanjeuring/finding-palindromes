9c184d9da2d37183dc900db9d4715dfba82e1740
goofvos <goofvos@live.nl>
2025-03-18 14:56:22 +0100

    resolved error

--- src/Data/Algorithms/Palindromes/LinearAlgorithm.hs
+++ src/Data/Algorithms/Palindromes/LinearAlgorithm.hs
@@ -84,7 +84,7 @@ moveCenterS
     input
     rightmost
     maximalPalindromesIn
-    maximalPalindromesIn'@(headq : tailq)
+    maximalPalindromesIn'
     nrOfCenters
         | nrOfCenters == 0 =
             -- the last centre is on the last element: try to extend the tail
@@ -107,7 +107,7 @@ moveCenterS
                     (rightmost + 1)
                     inputForExtend
                     newPalLength
-        | nrOfCenters - centerfactor == headq =
+        | nrOfCenters - centerfactor == p =
             {- The previous element in the centre list reaches exactly to the end of the
              last tail palindrome. Use the mirror property of palindromes to find the
              longest tail palindrome -}
@@ -120,17 +120,21 @@ moveCenterS
         | otherwise =
             {- move the centres one step and add the length of the longest palindrome to
             the centres -}
-            moveCenterS
-                antiReflexive
-                input
-                rightmost
-                (min headq (nrOfCenters - centerfactor) : maximalPalindromesIn)
-                tailq
-                (nrOfCenters - centerfactor)
+            case maximalPalindromesIn' of
+                (headq : tailq) ->
+                    moveCenterS
+                        antiReflexive
+                        input
+                        rightmost
+                        (min headq (nrOfCenters - centerfactor) : maximalPalindromesIn)
+                        tailq
+                        (nrOfCenters - centerfactor)
+                [] -> error "extendPalindromeS: empty sequence"
       where
         {- If type is anti-reflexive, we can skip centers on elements, so take steps of
         size 2. -}
         centerfactor = if antiReflexive then 2 else 1
+        (p : _) = maximalPalindromesIn'
 
 {- | After the current palindrome reached the end of the input vector, this function will
 find and return the final palindromes using the pal in pal property

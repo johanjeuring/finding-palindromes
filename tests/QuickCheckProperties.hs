a7d71ce3a728308e36c3d974da912a474798b620
goofvos <goofvos@live.nl>
2025-03-22 14:44:57 +0100

    added some properties

--- tests/QuickCheckProperties.hs
+++ tests/QuickCheckProperties.hs
@@ -18,6 +18,8 @@ import Test.QuickCheck (Arbitrary, Gen, Property, arbitrary, elements, forAll)
 instance Arbitrary DNA where
     arbitrary = elements [A, T, C, G]
 
+-- Property 1 ---------------------------------------------------------
+
 propValidPalindromeRangeAndTextPlain :: Property
 propValidPalindromeRangeAndTextPlain = propValidPalindromeRangeAndText VarPlain
 propValidPalindromeRangeAndTextText :: Property
@@ -47,6 +49,8 @@ checkPalRangeAndText (Palindrome _ _ palText (start, end)) originalString = palT
   where
     substringFromRange = take (end - start) (drop start originalString)
 
+-- Property 2 ---------------------------------------------------------
+
 -- | Check that a found palindrome is actually a palindrome
 propValidPalindromeReverse :: Settings -> Property
 propValidPalindromeReverse settings = forAll (arbitrary :: Gen [Char]) $ \originalString ->
@@ -58,10 +62,51 @@ propValidPalindromeReverse settings = forAll (arbitrary :: Gen [Char]) $ \origin
             (lengthMod settings)
             originalString
         )
-
 -- | Removes gap from palindrome, and unescapes, lowercases, input
 preprocessingGap :: Int -> String -> String
 preprocessingGap gapLength palindrome = undefined
   where
     len = length palindrome
     toRemove = if even len == even gapLength then gapLength else gapLength - 1
+
+-- Property 3 ---------------------------------------------------------
+
+propPalLength :: Settings -> Property
+propPalLength settings = forAll (arbitrary :: Gen [Char]) $ \originalString ->
+    all
+        (\pal -> length (palText pal) == palLength pal)
+        ( createCombinator
+            (variant settings)
+            (complexity settings)
+            (lengthMod settings)
+            originalString
+        )
+
+-- Property 4 ---------------------------------------------------------
+
+propPalCenterIndex :: Settings -> Property
+propPalCenterIndex settings = forAll (arbitrary :: Gen [Char]) $ \originalString ->
+    all
+        (\pal -> palCenterIndex pal == sumTuple (palRange pal))
+        ( createCombinator
+            (variant settings)
+            (complexity settings)
+            (lengthMod settings)
+            originalString
+        )
+
+sumTuple :: (Int, Int) -> Int
+sumTuple (p, q) = p + q
+
+-- Property 5 ---------------------------------------------------------
+
+propPalRange :: Settings -> Property
+propPalRange settings = forAll (arbitrary :: Gen [Char]) $ \originalString ->
+    all
+        (\pal -> fst (palRange pal) >= 0 && snd (palRange pal) <= palLength pal)
+        ( createCombinator
+            (variant settings)
+            (complexity settings)
+            (lengthMod settings)
+            originalString
+        )

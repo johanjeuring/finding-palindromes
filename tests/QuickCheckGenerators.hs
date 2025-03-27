3fd37247a773fedb50aeb8eac5fa41f1d8f3a639
rubenofzo <ruben16609@gmail.com>
2025-03-27 15:59:08 +0100

    added a string before and after the generated palindrome, added a option for 1-5 palInPal palindromes to the generator (instead of only single palInPals)

--- tests/QuickCheckGenerators.hs
+++ tests/QuickCheckGenerators.hs
@@ -5,36 +5,53 @@ import Test.QuickCheck.Gen (elements, genFloat)
 
 {-
 todo:
+done - add string before and after genPal
 - uneven pal
-- random stuff around pals
+done - add a random amount of pal in pals
 - look into aprox and gapped
+- somehow unit test all of this
 -}
--- generates either a string, palindrome or palInPal
+-- the maximum amount of palInPal depth the generated palindrome will have
+-- a Depth of 1 gives a palindrome with one level of palindrome (pallap)
+-- a Depth of 2 gives a palindrome with two levels of palindromes (pallappallap) etc
+maxPalInPalGeneration :: Float
+maxPalInPalGeneration = 5
+
+-- generates either a string, palindrome or palInPal palindrome with radnom characters around them
 genPalString :: Gen String
 genPalString = do
-    let notPalindrome = arbitrary :: Gen String
-        palindrome = generatePalindrome
-        _palInpal = genPalInPal
-    oneof [notPalindrome, palindrome, _palInpal]
+    randomStart <- arbitrary :: Gen String
+    palGenerator <-
+        oneof [arbitrary :: Gen String, generatePalindrome, genSinglePalInPal, genMultiPalInPal]
+    randomEnd <- arbitrary :: Gen String
+    return $ randomStart ++ palGenerator ++ randomEnd
+
+genMultiPalInPal :: Gen String
+genMultiPalInPal = do
+    randomString <- arbitrary :: Gen String
+    randomFloat <- genFloat
+    let -- Scale and round off float to generate a reasonable amount of palindromes
+        scaledFLoat = round $ maxPalInPalGeneration * randomFloat
+    return $ palInPal scaledFLoat randomString
 
 -- generates a palindrome with a palInPal property
-genPalInPal :: Gen String
-genPalInPal = do
+genSinglePalInPal :: Gen String
+genSinglePalInPal = do
     randomString <- arbitrary :: Gen String
     return $ palInPal 2 randomString
 
 -- generate a palindrome from string with Int amount of palindromes
--- an Int of 0 gives the input back, (pal)
--- an Int of 1 gives a palindrome with one level of palindrome (pallap)
--- an Int of 2 gives a palindrome with two levels of palindrome (pallappallap)
+-- a depth of 0 gives the input back, (pal)
+-- a depth of 1 gives a palindrome with one level of palindrome (pallap)
+-- a depth of 2 gives a palindrome with two levels of palindrome (pallappallap)
 palInPal :: Int -> String -> String
-palInPal i string =
-    case i of
+palInPal depth string =
+    case depth of
         0 -> string
-        _ -> palInPal (i - 1) $ string ++ reverse string
+        _ -> palInPal (depth - 1) $ string ++ reverse string
 
 -- generates a palindrome
 generatePalindrome :: Gen String
 generatePalindrome = do
-    txt <- arbitrary :: Gen String
-    return $ txt ++ reverse txt
+    randomString <- arbitrary :: Gen String
+    return $ palInPal 1 randomString

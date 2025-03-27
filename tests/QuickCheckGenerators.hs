ee0be9fc539ccb432d68273fa68816ea7e240cdf
rubenofzo <ruben16609@gmail.com>
2025-03-27 14:37:14 +0100

    made basic quick check generator

--- tests/QuickCheckGenerators.hs
+++ tests/QuickCheckGenerators.hs
@@ -1,28 +1,40 @@
 module QuickCheckGenerators where
-    
-import Test.QuickCheck (Gen, Property, arbitrary, forAll, listOf, oneof, generate)
-import Test.QuickCheck.Gen (genFloat, elements)
-{- 
-idea: generate a palindrome by randomly adding either characters or palindromes
-which will place either characters or palindromes with a given frequency to generate a final string
-for quickcheck we have:
-    oneof <list of generators>
-    frequency :: [(Int, Gen a)] -> Gen a
 
+import Test.QuickCheck (Gen, Property, arbitrary, forAll, generate, listOf, oneof)
+import Test.QuickCheck.Gen (elements, genFloat)
+
+{-
+todo:
+- uneven pal
+- random stuff around pals
+- look into aprox and gapped
 -}
--- builds a string of a random length by choosing either a character or a palindrome each time
+-- generates either a string, palindrome or palInPal
 genPalString :: Gen String
 genPalString = do
-    let
-        char = listOf (elements ['a'..'z'])
-        pal = generatePalindrome
-        --listLength = 100 * genFloat
-        --randomList = listOf listLength
-    oneof [char, pal]
+    let notPalindrome = arbitrary :: Gen String
+        palindrome = generatePalindrome
+        _palInpal = genPalInPal
+    oneof [notPalindrome, palindrome, _palInpal]
+
+-- generates a palindrome with a palInPal property
+genPalInPal :: Gen String
+genPalInPal = do
+    randomString <- arbitrary :: Gen String
+    return $ palInPal 2 randomString
+
+-- generate a palindrome from string with Int amount of palindromes
+-- an Int of 0 gives the input back, (pal)
+-- an Int of 1 gives a palindrome with one level of palindrome (pallap)
+-- an Int of 2 gives a palindrome with two levels of palindrome (pallappallap)
+palInPal :: Int -> String -> String
+palInPal i string =
+    case i of
+        0 -> string
+        _ -> palInPal (i - 1) $ string ++ reverse string
 
 -- generates a palindrome
-generatePalindrome:: Gen String
+generatePalindrome :: Gen String
 generatePalindrome = do
-    txt <- listOf (elements ['a'..'z'])
-    reverseTxt <- reverse txt
-    return txt ++ reverseTxt
+    txt <- arbitrary :: Gen String
+    return $ txt ++ reverse txt

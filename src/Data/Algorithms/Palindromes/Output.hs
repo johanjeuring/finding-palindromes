614c775141b77bbae3070e4b4b2bac3543196e2b
Qun Franken <52831560+ChinaGirlNL@users.noreply.github.com>
2025-04-07 16:50:06 +0200

    chore: Rewrite indicesInOutputWord to use textToWords'

--- src/Data/Algorithms/Palindromes/Output.hs
+++ src/Data/Algorithms/Palindromes/Output.hs
@@ -27,6 +27,7 @@ import Data.Algorithms.Palindromes.Palindrome
     )
 import Data.Algorithms.Palindromes.PreProcessing
     ( filterLetters'
+    , textToWords'
     )
 import Data.Char
     ( isAlphaNum
@@ -54,90 +55,26 @@ of the text of the word palindrome in the original string
 -}
 indicesInOutputWord :: (Int, Int) -> String -> (Int, Int)
 indicesInOutputWord (start', end') input
-    | start' >= length wordLengths =
-        (length input, length input)
-    | end' - start' > 0 = (start, end)
-    | otherwise = (start, start)
+    | start' >= length wordsWithIndices =
+        (maxIndex, maxIndex)
+    | end' - start' > 0 = (startIndex, endIndex)
+    | otherwise = (startIndex, startIndex)
   where
-    -- The start and end index of the found palindrome in the original string
-    start = startSpaces + otherAtFiltered V.! startSpaces
-    end = endSpaces + otherAtFiltered V.! (endSpaces - 1)
+    wordsWithIndices :: V.Vector ((Int, Int), String)
+    wordsWithIndices = textToWords' input
 
-    -- The number of spaces found before the start index
-    startSpaces :: Int
-    startSpaces = startIndices' V.! start' + spaces V.! start'
-    -- The number of spaces found before the end index
-    endSpaces :: Int
-    endSpaces = (endIndices' V.! (end' - 1) + spaces V.! (end' - 1)) + 1
+    maxIndex :: Int
+    maxIndex = length input
 
-    -- The number of spaces found before the start of each word
-    startIndices' :: V.Vector Int
-    startIndices' = V.fromList $ init $ scanl (+) 0 wordLengths
-    -- The number of spaces found before the end of each word
-    endIndices' :: V.Vector Int
-    endIndices' = V.fromList $ drop 1 $ scanl (+) (-1) wordLengths
+    firstWord :: ((Int, Int), String)
+    firstWord = wordsWithIndices V.! start'
+    lastWord :: ((Int, Int), String)
+    lastWord = wordsWithIndices V.! (end' - 1)
 
-    -- The number of consecutive spaces found from the previous word to the next word
-    spaces :: V.Vector Int
-    spaces = V.map snd $ V.filter isSpaceCount $ V.indexed spacesAt
-
-    -- The lengths of all words in the input string, including only letters and numbers
-    wordLengths :: [Int]
-    wordLengths = map length $ words $ filter (\a -> isAlphaNum a || isSpace a) input
-    -- The number of spaces until any index in the original input string
-    spacesAt :: V.Vector Int
-    spacesAt =
-        V.fromList $
-            drop 1 $
-                scanl
-                    (\acc x -> if isSpace x then acc + 1 else acc)
-                    0
-                    (filter (\x -> isAlphaNum x || isSpace x) input)
-
-    -- The number of "other" characters before every word
-    --   "other" is defined as any character that is not alphanumeric or whitespace
-    otherAtFiltered :: V.Vector Int
-    otherAtFiltered =
-        if V.null $
-            V.map fst $
-                V.filter
-                    (\(_, c) -> isAlphaNum c || isSpace c)
-                    (V.zip otherAt $ V.fromList input)
-            then V.fromList [0]
-            else
-                V.map fst $
-                    V.filter
-                        (\(_, c) -> isAlphaNum c || isSpace c)
-                        (V.zip otherAt $ V.fromList input)
-    -- The number of "other" characters before every input character
-    otherAt :: V.Vector Int
-    otherAt =
-        V.fromList $
-            drop 1 $
-                scanl
-                    (\acc x -> if (not . isAlphaNum) x && (not . isSpace) x then acc + 1 else acc)
-                    0
-                    input
-
-    -- A number at an index is a "space count" when it's a number that marks the number of spaces
-    --   belonging to a single space between two words.
-    -- Example:
-    -- The string "  . q we r" has the spaces
-    -- [1,2,2,3,3,4,4,4,5,5]
-    --          ^     ^   ^
-    -- The number 3, 4 and 5 mark the number of spaces before q, we and r respectively.
-    isSpaceCount :: (Int, Int) -> Bool
-    isSpaceCount (spaceIndex, numberOfSpaces) =
-        ( spaceIndex /= 0
-            && spaceIndex /= length spacesAt - 1
-            && spacesAt V.! (spaceIndex - 1) == numberOfSpaces
-            && spacesAt V.! (spaceIndex + 1) /= numberOfSpaces
-        )
-            || spaceIndex == length spacesAt - 1
-            || ( spaceIndex == 0
-                    && numberOfSpaces == 0
-                    && spacesAt V.! (spaceIndex + 1) /= numberOfSpaces
-               )
+    startIndex :: Int
+    startIndex = fst $ fst firstWord
+    endIndex :: Int
+    endIndex = snd (fst lastWord)
 
 -- | Takes a start and end index (exclusive) and returns the substring with those indices
 indicesToText :: (Int, Int) -> String -> String

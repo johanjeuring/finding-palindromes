4320a8e23dfc8ec30fcd117682710d1ebe2f199b
Rens de Wit <r.dewit1@students.uu.nl>
2025-04-29 18:12:41 +0200

    Chore: change unit tests for Insertion Deletion algorithm to use the correct data types.

--- tests/UTInsertionDeletionAlgorithm.hs
+++ tests/UTInsertionDeletionAlgorithm.hs
@@ -27,13 +27,43 @@ testSparsifyEdgeCase =
     TestCase $
         assertEqual
             "testSparsifyEdgeCase"
-            (sparsify [1, 0, -1, 0])
-            [1, 0, -1, 0]
+            (sparsify [Cell (0, 0) 1, Cell (0, 1) 0, Cell (0, 2) (-1), Cell (0, 3) 0])
+            [Cell (0, 0) 1, Cell (0, 1) 0, Cell (0, 2) (-1), Cell (0, 3) 0]
 
 testSparsifyComplex :: Test
 testSparsifyComplex =
     TestCase $
         assertEqual
             "testSparsifySimple"
-            (sparsify [1, 0, -1, -1, -1, -1, 0, 1, 0, -1, 0, 0, 0, -1, -2, -3, -4])
-            [1, 0, -1, -1, 0, 1, 0, -1, 0, 0, 0, -1, -1]
+            ( sparsify
+                [ Cell (0, 0) 1
+                , Cell (0, 1) 0
+                , Cell (0, 2) (-1)
+                , Cell (0, 3) (-1)
+                , Cell (0, 4) (-1)
+                , Cell (0, 5) (-1)
+                , Cell (0, 6) 0
+                , Cell (0, 7) 1
+                , Cell (0, 8) 0
+                , Cell (0, 9) (-1)
+                , Cell (0, 10) 0
+                , Cell (0, 11) 0
+                , Cell (0, 12) 0
+                , Cell (0, 13) (-1)
+                , Cell (0, 14) (-2)
+                , Cell (0, 15) (-3)
+                , Cell (0, 16) (-4)
+                ]
+            )
+            [ Cell (0, 0) 1
+            , Cell (0, 1) 0
+            , Cell (0, 2) (-1)
+            , Cell (0, 5) (-1)
+            , Cell (0, 6) 0
+            , Cell (0, 7) 1
+            , Cell (0, 8) 0
+            , Cell (0, 9) (-1)
+            , Cell (0, 10) 0
+            , Cell (0, 11) 0
+            , Cell (0, 12) 0
+            ]

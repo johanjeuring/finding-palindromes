a72fe118f7c1799235dc430bdec24694e6fcc4d0
Rens de Wit <r.dewit1@students.uu.nl>
2025-05-08 15:35:20 +0200

    Chore: delete tests for insertion deletion with DNA, because this feature will be added later.

--- tests/UTInsertionDeletionAlgorithm.hs
+++ tests/UTInsertionDeletionAlgorithm.hs
@@ -15,8 +15,6 @@ import qualified Data.Vector as V
 testListInsertionDeletionAlgorithm =
     [ testTeesZeroErrors
     , testTeesOneError
-    , testDNAZeroErrors
-    , testDNAOneError
     , testMississippiZeroErrors
     , testMississippiOneError
     , testMississippiTwoErrors
@@ -48,29 +46,6 @@ testTeesOneError =
             "testTeesOneError"
             [(0, 4)]
             (sort $ insertionDeletionAlgorithm 1 (V.fromList "tees"))
-
-{- | Test a small DNA sequence with zero errors. Note that for an empty maximal
-approximate palindrome, the start character index is the same as the end character index.
-Both are the index of the character directly following the empty string in question.
--}
-testDNAZeroErrors :: Test
-testDNAZeroErrors =
-    TestCase $
-        assertEqual
-            "testDNAZeroErrors"
-            [(0, 2), (2, 2), (3, 3)]
-            (sort $ insertionDeletionAlgorithm 0 (V.fromList [A, T, G, G]))
-
-{- | Test the small DNA sequence with one error. The output represents strings [A, T, G]
-and [G, G].
--}
-testDNAOneError :: Test
-testDNAOneError =
-    TestCase $
-        assertEqual
-            "testDNAOneError"
-            [(0, 3), (2, 4)]
-            (sort $ insertionDeletionAlgorithm 1 (V.fromList [A, T, G, G]))
 
 -- | Test the string "mississippi" with zero errors.
 testMississippiZeroErrors :: Test

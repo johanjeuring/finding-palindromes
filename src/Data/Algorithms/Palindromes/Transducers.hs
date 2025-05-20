f04e278a41a1aa54c7fbd5ee16e33522b933b24a
Rens de Wit <r.dewit1@students.uu.nl>
2025-05-20 10:44:03 +0200

    Chore: add comments and change variable names for Transducers to better explain what happens and why it works like it does.

--- src/Data/Algorithms/Palindromes/Transducers.hs
+++ src/Data/Algorithms/Palindromes/Transducers.hs
@@ -10,43 +10,50 @@ module Data.Algorithms.Palindromes.Transducers (TransduceExtractor (..), transdu
 import Data.List (scanl')
 
 {- | Data type containing functions to extract output from states with a special function
-that is called for the end state
+that is called for the end state.
 -}
 data TransduceExtractor state output = TransExtract
     { extractFunction :: state -> [output]
     , endFunction :: state -> [output]
     }
 
-{- | Create list of states by applying the stateFunction repeatedly to the initial state
-using the input list
+{- | Create list of states by doing a scanl using the statefunction, the initial state and
+the inputs list. The transduceExtract function can then extract information based on this
+list of states.
 -}
 transduceMap :: (state -> input -> state) -> state -> [input] -> [state]
 transduceMap stateFunction initialState inputs =
-    -- scanl' puts the initial state in the list, which we do not want.
+    {- scanl' puts the initial state in the list, which we do not want. So, we have to
+    drop the first element. -}
     drop 1 $ scanl' stateFunction initialState inputs
 
 {- Use the functions from the TransduceExtractor on the list of states to generate the
-output list -}
+outputs list. -}
 transduceExtract :: TransduceExtractor state output -> [state] -> [output]
 transduceExtract _ [] = []
 -- If you are at the final element, also apply the endFunction
-transduceExtract (TransExtract f e) [s] = f s ++ e s
-transduceExtract transExtract (s : x : t) =
-    extractFunction transExtract s ++ transduceExtract transExtract (x : t)
+transduceExtract (TransExtract{extractFunction = f, endFunction = e}) [s] = f s ++ e s
+transduceExtract transExtract@TransExtract{extractFunction = f} (s : x : t) =
+    f s ++ transduceExtract transExtract (x : t)
 
---------------------------------------------------
-
--- | Get the output from a transducer with 1 output.
+{- | Generates a list of states and extracts information from them using a single
+TransduceExtractor. Because of lazy evaluation, the mapping and extraction is done
+alternatingly. So, the inputs list only needs to be traversed once. This causes memory and
+time efficient evaluation on large or even infinite lists.
+-}
 transducel1
     :: (state -> input -> state)
     -> TransduceExtractor state output
     -> state
     -> [input]
     -> [output]
-transducel1 stateFunction transExtractor initialState input =
-    transduceExtract transExtractor $ transduceMap stateFunction initialState input
+transducel1 stateFunction transExtractor initialState inputs =
+    transduceExtract transExtractor $ transduceMap stateFunction initialState inputs
 
--- | Get the output from a transducer with 2 outputs.
+{- | The same as transducel1, but uses two TransduceExtractors. This way, you can get two
+different output lists (possibly of different lengths), using the same states for both, so
+you only have to transduceMap once.
+-}
 transducel2
     :: (state -> input -> state)
     -> TransduceExtractor state output0
@@ -54,6 +61,7 @@ transducel2
     -> state
     -> [input]
     -> ([output0], [output1])
-transducel2 statef tre0 tre1 ini inp = (transduceExtract tre0 states, transduceExtract tre1 states)
+transducel2 stateFunction transExtractor0 transExtractor1 initialState inputs =
+    (transduceExtract transExtractor0 states, transduceExtract transExtractor1 states)
   where
-    states = transduceMap statef ini inp
+    states = transduceMap stateFunction initialState inputs
